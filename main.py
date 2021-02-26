18469135599ae2b7d612031f5efa3319a25a5d88
taarusshwd <taarussh21wadhwa@gmail.com>
2021-02-26 18:55:57 +0530

    update

--- main.py
+++ main.py
@@ -21,9 +21,26 @@ headers = {
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
-data = json.loads(response.txt)
-matches = data['matchList']
-print(matches)
+data = json.loads(response.text)
+#matches = data['matchList']
+#print(type(data['matches']))
+
+for match in data['matchList']['matches']:
+    if match['id'] == 49840:
+        venue = match['venue']['name']
+        status = match['status']
+        summary = match['currentMatchState']
+        score = match['scores']
+        break
+
+print(venue)
+print(status)
+print(summary)
+for i in score:
+    print(i + ": " + score[i])
+        
+
+        
 
 #print(response.text)
 
