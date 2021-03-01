43b449e6c2aa29ab78861d5ad7133f298e2aff53
taarusshwd <taarussh21wadhwa@gmail.com>
2021-03-01 23:34:58 +0530

    update - notification

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests, argparse
 import json
+import telebot
+from telethon.sync import TelegramClient 
+from telethon.tl.types import InputPeerUser, InputPeerChannel 
+from telethon import TelegramClient, sync, events 
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -33,15 +38,63 @@ for match in data['matchList']['matches']:
         score = match['scores']
         break
 
-print(venue)
+def telegram_bot_sendtext(bot_message):
+    
+    bot_token = '1546857499:AAG_aFZeRhR-bLibgdTwCgLzQDZ9NriQoVI'
+    bot_chatID = '1081769492'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+    
+
+test = telegram_bot_sendtext(venue)
+print(test)
+
+
+"""api_id = '3664885'
+api_hash = '597eb2483c1e172e8e52d40461444f40'
+token = '1546857499:AAG_aFZeRhR-bLibgdTwCgLzQDZ9NriQoVI'
+
+phone = '+919886882121'
+
+client = TelegramClient('session', api_id, api_hash)
+
+client.connect() 
+
+if not client.is_user_authorized(): 
+   
+    client.send_code_request(phone) 
+      
+    # signing in the client 
+    client.sign_in(phone, input('Enter the code: ')) 
+
+try: 
+    # receiver user_id and access_hash, use 
+    # my user_id and access_hash for reference 
+    receiver = InputPeerUser('user_id', 'user_hash') 
+  
+    # sending message using telegram client 
+    client.send_message(receiver, message = venue, parse_mode='html') 
+except Exception as e: 
+      
+    # there may be many error coming in while like peer 
+    # error, wwrong access_hash, flood_error, etc 
+    print(e)
+  
+# disconnecting the telegram session  
+client.disconnect() 
+"""
+
+
+
+"""print(venue)
 print(status)
 print(summary)
 for i in score:
     print(i + ": " + score[i])
         
-
-        
-
 #print(response.text)
 
-#id = 49839, 2730
+#id = 49839, 2730"""
