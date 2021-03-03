344d5c4df17cf84301ab53ff854b1aceeeb45140
taarusshwd <taarussh21wadhwa@gmail.com>
2021-03-03 18:18:02 +0530

    updated

--- main.py
+++ main.py
@@ -14,30 +14,34 @@ parser.add_argument(
     default = 2730,
     help = 'enter the series id/match id'
 )
-
 args = parser.parse_args()
 
-url = "https://dev132-cricket-live-scores-v1.p.rapidapi.com/matchseries.php"
-id = args.id
-querystring = {"seriesid": id}
-headers = {
-    'x-rapidapi-key': "3f002d2a37mshbd5ff13d303a772p114fe9jsn3c632d48eeef",
-    'x-rapidapi-host': "dev132-cricket-live-scores-v1.p.rapidapi.com"
-    }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+def generate_message_data():
+    url = "https://dev132-cricket-live-scores-v1.p.rapidapi.com/matchseries.php"
+    id = args.id
+    querystring = {"seriesid": id}
+    headers = {
+        'x-rapidapi-key': "3f002d2a37mshbd5ff13d303a772p114fe9jsn3c632d48eeef",
+        'x-rapidapi-host': "dev132-cricket-live-scores-v1.p.rapidapi.com"
+        }
 
-data = json.loads(response.text)
-#matches = data['matchList']
-#print(type(data['matches']))
+    response = requests.request("GET", url, headers=headers, params=querystring)
 
-for match in data['matchList']['matches']:
-    if match['id'] == 49840:
-        venue = match['venue']['name']
-        status = match['status']
-        summary = match['currentMatchState']
-        score = match['scores']
-        break
+    data = json.loads(response.text)
+    #matches = data['matchList']
+    #print(type(data['matches']))
+
+    for match in data['matchList']['matches']:
+        if match['id'] == 49840:
+            venue = match['venue']['name']
+            status = match['status']
+            summary = match['currentMatchState']
+            score = match['scores']
+            break
+
+    return venue, status, summary, score
+
 
 def telegram_bot_sendtext(bot_message):
     
@@ -48,12 +52,19 @@ def telegram_bot_sendtext(bot_message):
     response = requests.get(send_text)
 
     return response.json()
-    
+
+
+
 while(True):
-    time.sleep(3)
+    time.sleep(10)
+    venue, status, summary, scores = generate_message_data()
     test = telegram_bot_sendtext(venue)
+    time.sleep(1)
     test = telegram_bot_sendtext(status)
+    time.sleep(1)
     test = telegram_bot_sendtext(summary)
+    time.sleep(1)
+
 
 
 
@@ -69,3 +80,8 @@ for i in score:
 #print(response.text)
 
 #id = 49839, 2730"""
+
+#1 Print scores
+#2 Isolate the functions; this main.py should call other functions
+#3 make another function to send the notifs: shouldn't be happening in while loop
+#4 more functionalities
